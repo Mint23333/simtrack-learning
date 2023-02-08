7298fc1e02e5a6cf3a4563f788199385cfdcdef3
Mint23333 <124549809+Mint23333@users.noreply.github.com>
2023-02-08 11:47:03 +0800

    Update box_ops.py

--- lib/utils/box_ops.py
+++ lib/utils/box_ops.py
@@ -4,28 +4,28 @@ import numpy as np
 
 
 def box_cxcywh_to_xyxy(x):
-    x_c, y_c, w, h = x.unbind(-1)
+    x_c, y_c, w, h = x.unbind(-1) #x_c, y_c, w, h为平均的横纵坐标，宽度和长度
     b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
-         (x_c + 0.5 * w), (y_c + 0.5 * h)]
+         (x_c + 0.5 * w), (y_c + 0.5 * h)] #b为左下和右上坐标
     return torch.stack(b, dim=-1)
 
 
-def box_xywh_to_xyxy(x):
-    x1, y1, w, h = x.unbind(-1)
-    b = [x1, y1, x1 + w, y1 + h]
+def box_xywh_to_xyxy(x): 
+    x1, y1, w, h = x.unbind(-1) #x1, y1, w, h为左下角坐标和长宽
+    b = [x1, y1, x1 + w, y1 + h] #b为左下和右上坐标
     return torch.stack(b, dim=-1)
 
 
 def box_xyxy_to_xywh(x):
     x1, y1, x2, y2 = x.unbind(-1)
-    b = [x1, y1, x2 - x1, y2 - y1]
+    b = [x1, y1, x2 - x1, y2 - y1] #b为左下坐标和长宽
     return torch.stack(b, dim=-1)
 
 
 def box_xyxy_to_cxcywh(x):
     x0, y0, x1, y1 = x.unbind(-1)
     b = [(x0 + x1) / 2, (y0 + y1) / 2,
-         (x1 - x0), (y1 - y0)]
+         (x1 - x0), (y1 - y0)]    #为平均的横纵坐标，宽度和长度
     return torch.stack(b, dim=-1)
 
 
@@ -33,7 +33,7 @@ def box_xyxy_to_cxcywh(x):
 '''Note that this function only supports shape (N,4)'''
 
 
-def box_iou(boxes1, boxes2):
+def box_iou(boxes1, boxes2): 
     """
 
     :param boxes1: (N, 4) (x1,y1,x2,y2)
@@ -41,24 +41,24 @@ def box_iou(boxes1, boxes2):
     :return:
     """
     area1 = box_area(boxes1) # (N,)
-    area2 = box_area(boxes2) # (N,)
+    area2 = box_area(boxes2) # (N,) 计算两个框面积
 
-    lt = torch.max(boxes1[:, :2], boxes2[:, :2])  # (N,2)
-    rb = torch.min(boxes1[:, 2:], boxes2[:, 2:])  # (N,2)
+    lt = torch.max(boxes1[:, :2], boxes2[:, :2])  # (N,2)  
+    rb = torch.min(boxes1[:, 2:], boxes2[:, 2:])  # (N,2) 
 
     wh = (rb - lt).clamp(min=0)  # (N,2)
-    inter = wh[:, 0] * wh[:, 1]  # (N,)
+    inter = wh[:, 0] * wh[:, 1]  # (N,) 两个框的交集面积
 
-    union = area1 + area2 - inter
+    union = area1 + area2 - inter #两个框的并集面积
 
-    iou = inter / union
+    iou = inter / union 
     return iou, union
 
 
 '''Note that this implementation is different from DETR's'''
 
 
-def generalized_box_iou(boxes1, boxes2):
+def generalized_box_iou(boxes1, boxes2): #boxes1, boxes2为预测框和实物框
     """
     Generalized IoU from https://giou.stanford.edu/
 
@@ -70,8 +70,8 @@ def generalized_box_iou(boxes1, boxes2):
     # degenerate boxes gives inf / nan results
     # so do an early check
     # try:
-    assert (boxes1[:, 2:] >= boxes1[:, :2]).all()
-    assert (boxes2[:, 2:] >= boxes2[:, :2]).all()
+    assert (boxes1[:, 2:] >= boxes1[:, :2]).all() 
+    assert (boxes2[:, 2:] >= boxes2[:, :2]).all() #？两个框都要保证左下角坐标小于右上角的
     iou, union = box_iou(boxes1, boxes2) # (N,)
 
     lt = torch.min(boxes1[:, :2], boxes2[:, :2])
@@ -80,10 +80,10 @@ def generalized_box_iou(boxes1, boxes2):
     wh = (rb - lt).clamp(min=0)  # (N,2)
     area = wh[:, 0] * wh[:, 1] # (N,)
 
-    return iou - (area - union) / area, iou
+    return iou - (area - union) / area, iou #计算并返回GIOU
 
 
-def giou_loss(boxes1, boxes2):
+def giou_loss(boxes1, boxes2): #
     """
 
     :param boxes1: (N, 4) (x1,y1,x2,y2)
